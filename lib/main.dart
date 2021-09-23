9df8f0855b81cc3f2eae5854ef234b2031613935
ramanajaneyulu velugu <rama@ramanajaneyulus-MacBook-Pro.local>
2021-09-23 15:55:40 +0530

    first comit

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MusicApp());
+}
+
+class MusicApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
